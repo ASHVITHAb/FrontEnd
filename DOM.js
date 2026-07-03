5e1eb47e97922bbb69ee7a1c5967fe58791ab60f
ASHVITHAb <ashvithameena@gmail.com>
2026-07-03 22:01:24 +0530

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+          <link rel="stylesheet" href="./domyt.css">
+</head>
+
+<body>
+          <p id="one">Hi i am ashvi</p>
+          <p id="two">I complete my higher studys</p>
+</body>
+<script>
+          let ash = document.getElementById("one")
+          console.log(ash);
+          let ash1 = document.getElementById("two")
+          console.log(ash1.textContent);
+
+
+//output
+/*<p id="one">Hi i am ashvi</p> --> one
+I complete my higher studys -->two*/
+
+</script>
+
+</html>
