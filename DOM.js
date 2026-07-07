2d879a2b171b099b846fe9d213bba5f4546ce6d8
ASHVITHAb <ashvithameena@gmail.com>
2026-07-07 23:18:58 +0530

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -31,7 +31,49 @@ I complete my higher studys -->two*/
 </script>
 // onclick event
 </html>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>onclick Function</title>
+</head>
+
+<body>
+          <!-- <h1 id="h1">Hello World</h1>
+          <button onclick="change()">change</button> -->
+          <input id="num1">
+          <input id="num2">
+          <button onclick="resultfunction()">Add</button>
+          <p id="Result">Result</p>
+</body>
+<script>
+          // let ash = document.getElementById("h1")
+          // console.log(h1.textContent);
+
+          // function change() {
+
+          //           h1.textContent = "Bye world"
+          // }
+          // selecting inputbox1
+          //selecting inputbox2
+          var ash1 = document.getElementById("num1")
+          var ash2 = document.getElementById("num2")
+          //selecting result
+          var result = document.getElementById("Result")
+          function resultfunction() {
+                    console.log(ash1.value);
+                    console.log(ash2.value);
+                    total = Number(ash1.value) + Number(ash2.value)
+                    console.log(total);
+
+          }
+
+
+</script>
+
+</html>
 
 <!DOCTYPE html>
 <html lang="en">
