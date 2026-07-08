022bb983f09550df99badd05276d3ebd406de7e9
ASHVITHAb <ashvithameena@gmail.com>
2026-07-08 19:03:33 +0530

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -75,7 +75,55 @@ I complete my higher studys -->two*/
 
 </html>
 
-          //output for this code is visible in console not in webpage
+          //output for this code is visible in console not in webpage 
+
+          <!DOCTYPE html>
+<html lang="en">
+
+<head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>onclick Function</title>
+</head>
+
+<body>
+          <!-- <h1 id="h1">Hello World</h1>
+          <button onclick="change()">change</button> -->
+          <input id="num1" value="2">
+          <input id="num2" value="2">
+          <button onclick="resultfunction()">Add</button>
+          <p id="result">Result</p>
+</body>
+<script>
+          // let ash = document.getElementById("h1")
+          // console.log(h1.textContent);
+
+          // function change() {
+
+          //           h1.textContent = "Bye world"
+          // }
+          // selecting inputbox1
+          //selecting inputbox2
+          var ash1 = document.getElementById("num1")
+          var ash2 = document.getElementById("num2")
+          //selecting result
+          var result = document.getElementById("result")
+          var ash1value = Number(ash1.value)
+          var ash2value = Number(ash2.value)
+          var total = ash1value + ash2value
+          function resultfunction() {
+
+                    result.textContent = total
+
+
+          }
+
+
+</script>
+
+</html>
+
+          //output will show in html webpage
 
 <!DOCTYPE html>
 <html lang="en">
