97c1ad62f8d5472e84de06eeceabe6dbdf33f98c
ASHVITHAb <ashvithameena@gmail.com>
2026-07-09 21:49:42 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,19 @@
+//1. lenght
+// systeax
+string.length
+// Example
+let name = "JavaScript";
+
+console.log(name.length);
+// o/p 10
+// 2. toUpperCase()
+//syntax
+string.toUpperCase()
+//Example
+let city = "chennai";
+
+console.log(city.toUpperCase());
+// o/p CHENNAI
+
+// 3. toLowerCase()
+// syntax
