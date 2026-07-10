eab565b7531632a37ef168d2bbb1c4f6da80257e
ASHVITHAb <ashvithameena@gmail.com>
2026-07-10 22:15:03 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -5,4 +5,26 @@ console.log(numbers);
 // Output
 [10, 20, 30, 40]
 
-2. Index Starts from 0
+// 2. Index Starts from 0
+let fruits = ["Apple", "Mango", "Orange"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// Output
+Apple
+Mango
+Orange
+
+// 3. Arrays Can Store Different Data Types
+let data = [
+    "Ashvitha",
+    23,
+    true,
+    95.5
+];
+
+console.log(data);
+
+
